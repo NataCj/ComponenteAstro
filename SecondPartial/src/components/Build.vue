74412cce6087bbcd6200e1e96d74ff867f3fd582
NataCj <mncardenasj@ufpso.edu.co>
2024-05-21 21:21:59 -0500

    first commit

---
+++ SecondPartial/src/components/Build.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref, computed } from "vue";
+const checked = ref(false);
+const count = ref(0);
+const items = {"1":"Tema 1","2":"Tema 2","3":"Tema 3","4":"Tema 4"};
+const checked1 ={"1":ref(false),"2":ref(false),"3":ref(false),"4":ref(false)}
+
+function message1(item){
+    let dos;
+    if(checked1[item]){
+        dos=false;
+        list[item]="+"
+    }else{
+        dos=true;
+        list[item]="-"
+    }
+    return dos;
+}
+
+function estado(item){
+    if(checked.value){
+
+    }else{
+
+    }
+}
+function validar(item){
+    if(item==items.value){
+        checked = message1(item);
+    }
+}
+
+</script>
+<template>
+    <div class="w-1/2 h-1/2 text-blue-900 flex flex-col" v-for="(item, index) in items">
+        
+        <button class="bg-blue-300 flex rounded-lg justify-evenly m-3 p-5 w-full text-left" @click="validar(item), estado(item)">    
+            {{ items[index] }} 
+            <div v-if="estado(item)" class="">-</div>
+            <div v-else>+</div>
+            
+        </button>
+        <div v-show="counter[item] > 0"  class="bg-blue-200 h-44 rounded-lg justify-evenly m-3 p-5 w-full text-left">
+            {{ "texto del tema " + items[index] }}
+        </div>
+
+    </div>
+    
+</template>
---
+++ SecondPartial/src/components/Lista.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue"
+
+const indice = ref(0)
+
+const temas = [{title: 'Tema 1',desc: 'texto tema 1'}
+    ,{title: 'Tema 2',desc: 'texto tema 2'}
+    ,{title: 'Tema 3',desc: 'texto tema 3'}
+    ,{title: 'Tema 4',desc: 'texto tema 4'}]
+
+function changeIndex(index) {
+    if (indice.value == index) {
+        indice.value = -1
+    } else {
+        indice.value = index
+    }
+}
+
+</script>
+
+<template>
+    <main class="h-screen w-full flex justify-center items-center">
+        <section class="bg-blue-300 w-full w-5xl flex flex-col gap-4 p-3">
+            <div class="flex flex-col gap-2" v-for="(tema, index) in temas">
+                <button @click="changeIndex(index)" class="bg-blue-100 flex  justify-around px-5 py-2 select-none">
+                    <p>
+                        {{ tema.title }}
+                    </p>
+
+                    <p class="text-xl" v-if="index != indice">+</p>
+                    <p class="text-2xl" v-else>-</p>
+                </button>
+
+                <div class="min-h-40 bg-slate-100 p-4" v-if="index == indice">
+                    {{ tema.desc }}
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
