4f43b9fbfd7ba903eb209e8ca2e8f20a584eab96
NataCj <mncardenasj@ufpso.edu.co>
2024-06-19 05:10:51 -0500

    add pokemons y target

---
+++ PracticaExamenFinal/src/components/vue/Target.vue
@@ -0,0 +1,58 @@
+<script setup></script>
+<template>
+    <section>
+        <div class="w-[500px] mx-auto my-[20%] bg-yellow-600 p-3 rounded-lg">
+            <div class="p-8 bg-slate-200 rounded-lg">
+                <div class="w-[90%] space-x-5 flex">
+                    <div class="w-[20%]">
+                        <img class="rounded-full"
+                            src="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1718668800&semt=ais_user"
+                            alt="">
+                    </div>
+                    <div class="w-[90%] font-semibold bg-blue-400">
+                        <div class="w-[100%] ">
+                            <h1 class="text-lg font-extrabold uppercase">Nameee</h1>
+                            <span class="inline-block  bg-red-100 mb-2 ">Email:
+                                <p class=" ml-2 text-gray-500">
+                                    Lorem ipsum dolor
+                                </p>
+                            </span>
+                            <span class="inline-block bg-red-100 mb-2 ">Location:
+                                <p class=" ml-2 text-gray-500">
+                                    Lorem ipsum dolor
+                                </p>
+                            </span>
+                            <span class="inline-block  bg-red-100 mb-2 ">Ocupation:
+                                <p class=" ml-2 text-gray-500">
+                                    Lorem ipsum dolor
+                                </p>
+                            </span>
+                            <span class="inline-block bg-red-100 mb-2 ">About me:
+                                <p class=" ml-2  text-gray-500 bg-red-400 w-[300px]">
+                                    Lorem ipsum dolor Lorem ipsum, Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae sunt tempore distinctio similique reiciendis nulla debitis, sequi facere mollitia odio nemo natus ab, cum eaque velit quis fuga aspernatur! Laborum.
+                                </p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+
+
+
+<!-- <template>
+    <section class="">
+        <div class="max-w-3xl bg-yellow-500 rounded-xl mx-auto my-[20%]">
+            <div class=" flex w-[95%] h-[300px] py-4  mx-auto  bg-red-400 ">
+                ajdnbf
+            </div>
+        </div>
+    </section>
+</template> -->
